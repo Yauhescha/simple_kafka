a848bfbdd4795e38d62ec9bb74920dcf49584e70
Yauhescha <evgescha.maslov007@yandex.ru>
2022-05-23 19:14:14 +0300

    Work variant

--- src/main/java/com/example/simple_kafka/SimpleKafkaApplication.java
+++ src/main/java/com/example/simple_kafka/SimpleKafkaApplication.java
@@ -2,9 +2,18 @@ package com.example.simple_kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@EnableKafka
 public class SimpleKafkaApplication {
+    @KafkaListener(topics="msg")
+    public void msgListener(String msg){
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println(msg);
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleKafkaApplication.class, args);
