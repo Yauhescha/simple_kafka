906a4f9590845162e617c1d8024eb827eea60d45
Yauhescha <evgescha.maslov007@yandex.ru>
2022-05-29 23:20:45 +0300

    subscribe one listener to three themes

--- src/main/java/com/example/simple_kafka/listener/MessageListener.java
+++ src/main/java/com/example/simple_kafka/listener/MessageListener.java
@@ -1,8 +1,6 @@
 package com.example.simple_kafka.listener;
 
-import com.example.simple_kafka.dto.Customer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -21,22 +19,8 @@ public class MessageListener {
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
 
-    private KafkaConsumer<String, Customer> kafkaConsumer;
-    private static boolean startedSimpleListen = false;
-    private static boolean startedAvroListen = false;
-
-    @KafkaListener(topics = "msg")
+    @KafkaListener(topics = {"msg", "CustomerCountry", "justTopic"})
     public void msgListener(String msg) {
-        writeMessage(msg);
-    }
-
-    @KafkaListener(topics = "CustomerCountry")
-    public void bookStaticListener(String msg) {
-        writeMessage(msg);
-    }
-
-    @KafkaListener(topics = "justTopic")
-    public void bookMessageWithProps(String msg) {
         writeMessage(msg);
     }
 
